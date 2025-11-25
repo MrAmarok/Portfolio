88d2c7c4c83da2e9259c40be111e1896200a7fcf
Marc MENDIA <114486413+MrAmarok@users.noreply.github.com>
2025-11-25 03:20:10 +0100

    Update GitHub link in social.ts

--- utils/social.ts
+++ utils/social.ts
@@ -7,6 +7,6 @@ export const SocialLink = [
     link: 'https://www.linkedin.com/in/marc-mendia/',
     backgroundColor: '#0077B5',
   },
-  { icon: faGithub, link: 'https://github.com/Dvaking', backgroundColor: '#333' },
+  { icon: faGithub, link: 'https://github.com/MrAmarok', backgroundColor: '#333' },
   { icon: faFile, link: 'cv.jpg', backgroundColor: 'red' },
 ];
